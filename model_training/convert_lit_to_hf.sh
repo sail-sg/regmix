adf02d8017f4d4403053130de506b6ab799d049a
SivilTaram <qian.liu@buaa.edu.cn>
2024-07-25 16:36:28 +0800

    clean up tthe convert script; add a config generation script

--- model_training/convert_lit_to_hf.sh
+++ model_training/convert_lit_to_hf.sh
@@ -6,4 +6,9 @@ export INP_FOLDER="lit_checkpoint_folder"
 export FILE_NAME="iter-025000-ckpt.pth"
 export OUT_FOLDER="converted_huggingface_model_folder"
 
+# convert the model into Huggingface compatible format, and save the config.json
 python convert_lit_checkpoint.py --checkpoint_name "$FILE_NAME" --inp_dir "$INP_FOLDER" --out_dir "$OUT_FOLDER" --model_name $ARCH_NAME
+
+# copy tokenizer and config into the new folder
+# WARNING: if you use a different tokenizer, you need to modify the folder
+cp -r preprocess/tokenizer/gptneox/* "$OUT_FOLDER"
